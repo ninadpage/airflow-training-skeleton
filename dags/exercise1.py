ead41ff8fa694330427348a470566d26bcaa912b
Ninad Page <ninad1902@gmail.com>
2020-01-31 11:38:06 +0100

    Fix exercise 1

--- dags/exercise1.py
+++ dags/exercise1.py
@@ -1,15 +1,16 @@
 from datetime import timedelta
 
+import airflow
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
 
 
 default_args = {
     'owner': 'Airflow',
-    'start_date': None,
+    'start_date': airflow.utils.dates.days_ago(2),
 }
 
-with DAG(dag_id='exercise_1', default_args=default_args, schedule_interval='0 0 * * *',
+with DAG(dag_id='exercise_1', default_args=default_args, schedule_interval=None,
          dagrun_timeout=timedelta(minutes=60)) as dag:
     t1 = DummyOperator(task_id='task1')
     t2 = DummyOperator(task_id='task2')
